7c0e86f9d7ebb63abe4ed2b54bdd81e69ee743d6
Karaliou Kiryl <karalev.kirill@gmail.com>
2016-02-10 03:40:30 +0300

    No_de to node in BT file

--- binary-tree.js
+++ binary-tree.js
@@ -9,13 +9,13 @@ class BinaryTree {
         var root = this.root;
         //case when there's no root
         if(!root){
-            this.root = new No_de(data);
+            this.root = new Node(data);
             return;
         }
 
         var currentNode = root;
-        var newNode = new No_de(data); 
-        //if the value is less than the current No_de's
+        var newNode = new Node(data); 
+        //if the value is less than the current Node's
         while(currentNode){
             if(data < currentNode.data){
                 if(!currentNode.left){
@@ -26,7 +26,7 @@ class BinaryTree {
                     currentNode = currentNode.left;
                 }
             }
-            //if the value is greater than the current No_de's
+            //if the value is greater than the current Node's
             else{
                 if(!currentNode.right){
                     currentNode.right = newNode;
@@ -43,14 +43,14 @@ class BinaryTree {
         var found       = false;
         var current     = this.root
 
-        //make sure there's a No_de to search
+        //make sure there's a Node to search
         while(!found && current){
 
-            //if the value is less than the current No_de's, go left
+            //if the value is less than the current Node's, go left
             if (data < current.data){
                 current = current.left;
 
-            //if the value is greater than the current No_de's, go right
+            //if the value is greater than the current Node's, go right
             } else if (data > current.data){
                 current = current.right;
 
@@ -60,7 +60,7 @@ class BinaryTree {
             }
         }
 
-        // true if the No_de was found
+        // true if the Node was found
         return found;
 	}
 
@@ -69,19 +69,19 @@ class BinaryTree {
         var parent      = null;
         var current     = this.root;
         var childCount;
-        var replacement = new No_de(0);
-        var replacementParent = new No_de(0);
-        var replacementMax = new No_de(0);
+        var replacement = new Node(0);
+        var replacementParent = new Node(0);
+        var replacementMax = new Node(0);
 
-        //make sure there's a No_de to search
+        //make sure there's a Node to search
         while(!found && current){
 
-            //if the value is less than the current No_de's, go left
+            //if the value is less than the current Node's, go left
             if (data < current.data){
                 parent = current;
                 current = current.left;
 
-            //if the value is greater than the current No_de's, go right
+            //if the value is greater than the current Node's, go right
             } else if (data > current.data){
                 parent = current;
                 current = current.right;
@@ -92,7 +92,7 @@ class BinaryTree {
             }
         }
 
-        //only proceed if the No_de was found(!!!)
+        //only proceed if the Node was found(!!!)
         if (found){
             //figure out how many children
             childCount = (current.left !== null ? 1 : 0) + 
@@ -119,14 +119,14 @@ class BinaryTree {
                         //...maybe
                         replacement = this.root.left;
 
-                        //find the right-most leaf No_de to be 
+                        //find the right-most leaf Node to be 
                         //the real new root
                         while (replacement.right !== null){
                             replacementParent = replacement;
                             replacement = replacement.right;
                         }
 
-                        //it's not the first No_de on the left
+                        //it's not the first Node on the left
                         if (replacementParent !== null){
 
                             //remove the new root from it's 
@@ -190,7 +190,7 @@ class BinaryTree {
                         replacementParent.left = current.left;
                         replacementParent.right = current.right;
                         
-                        //find the right-most No_de
+                        //find the right-most Node
                         while(replacement.right !== null){
                             replacementMax = replacement;
                             replacement = replacement.right;
@@ -216,20 +216,20 @@ class BinaryTree {
 	}
     
     traverse(process){
-        function inOrder(No_de){
-            if (No_de){
+        function inOrder(Node){
+            if (Node){
 
                 //traverse the left subtree
-                if (No_de.left !== null){
-                    inOrder(No_de.left);
+                if (Node.left !== null){
+                    inOrder(Node.left);
                 }            
 
-                //call the process method on this No_de
-                process.call(this, No_de);
+                //call the process method on this Node
+                process.call(this, Node);
 
                 //traverse the right subtree
-                if (No_de.right !== null){
-                    inOrder(No_de.right);
+                if (Node.right !== null){
+                    inOrder(Node.right);
                 }
             }
         }
@@ -240,19 +240,19 @@ class BinaryTree {
 
 	size() {
         var length = 0;
-        function inOrder(No_de){
-            if (No_de){
+        function inOrder(Node){
+            if (Node){
 
                 //traverse the left subtree
-                if (No_de.left !== null){
-                    inOrder(No_de.left);
+                if (Node.left !== null){
+                    inOrder(Node.left);
                 }            
 
                 length++;
 
                 //traverse the right subtree
-                if (No_de.right !== null){
-                    inOrder(No_de.right);
+                if (Node.right !== null){
+                    inOrder(Node.right);
                 }
             }
         }
